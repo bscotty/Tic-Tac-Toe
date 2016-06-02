9197963f27b0e24904febfefad0c9c68921ae906
Bryan Scott <bryan@pajato.com>
2016-06-02 17:28:01 -0400

    # Latest Changes (June 2, 2016)
    Refactoring the previous **MainActivity.java** into two specific pieces: a game engine (**MainActivity.java**) and specific board fragment (**TTTFragment.java**)
    
    ## Changed Files
    
    **MainActivity.java**
    * Now functions as the game engine, keeping track of the current turn, and the turns already taken by the players.
    * Keeps ***onCreate***, ***sendMessage***, and the onClick methods which are described in the XML (NOTE: these onClick methods are now described in **ttt_layout.xml**)
    * Snackbar notifications are now being used instead of Toast notifications
    * Member variables renamed to adhere to convention
    
    **activity_main.xml**
    * The meat of the layout is removed from **activity_main.xml** and into **ttt_layout.xml**
    * Only a fragment container remains, which will be used to store the current game fragment.
    
    **AndroidManifest.xml**
    * Changed so that whenever the orientation changes, instead of destroying and recreating the activity, we only cause a configChanges event
    
    ## New Files
    
    **TTTFragment.java**
    * A ***Fragment*** class that interprets the message commands that are enumerated by the game engine in **MainActivity.java**
    * Minor refactoring of previous methods that were stored in the old **MainActivity.java**
    * Snackbar notifications are now being used instead of Toast notifications
    * Member variables renamed to adhere to convention
    
    **ttt_layout.xml**
    * Essentially identical to the previous **activity_main.xml** layout file
    * Winner's Textview now aligns itself to the bottom of the board as opposed to the bottom of the screen, to avoid overlapping with the new Snackbox notifications
    
    **/app/build.gradle**
    * Included the new dependency for the Snackbox notifications.
    
    **build.gradle**
    * Included some new shortcuts.

--- README.md
+++ README.md
@@ -1,30 +1,68 @@
 # Tic-Tac-Toe
 
-##Latest Changes (May 31, 2016):
+# Latest Changes (June 2, 2016)
+
+Refactoring the previous **MainActivity.java** into two specific pieces: a game engine (**MainActivity.java**) and specific board fragment (**TTTFragment.java**)
+
+## Changed Files
+
+**MainActivity.java**
+* Now functions as the game engine, keeping track of the current turn, and the turns already taken by the players.
+* Keeps ***onCreate***, ***sendMessage***, and the onClick methods which are described in the XML (NOTE: these onClick methods are now described in **ttt_layout.xml**)
+* Snackbar notifications are now being used instead of Toast notifications
+* Member variables renamed to adhere to convention
+
+**activity_main.xml**
+* The meat of the layout is removed from **activity_main.xml** and into **ttt_layout.xml**
+* Only a fragment container remains, which will be used to store the current game fragment.
+
+**AndroidManifest.xml**
+* Changed so that whenever the orientation changes, instead of destroying and recreating the activity, we only cause a configChanges event
+
+## New Files
+
+**TTTFragment.java**
+* A ***Fragment*** class that interprets the message commands that are enumerated by the game engine in **MainActivity.java**
+* Minor refactoring of previous methods that were stored in the old **MainActivity.java**
+* Snackbar notifications are now being used instead of Toast notifications
+* Member variables renamed to adhere to convention
+
+**ttt_layout.xml**
+* Essentially identical to the previous **activity_main.xml** layout file
+* Winner's Textview now aligns itself to the bottom of the board as opposed to the bottom of the screen, to avoid overlapping with the new Snackbox notifications
+
+**/app/build.gradle**
+* Included the new dependency for the Snackbox notifications.
+
+**build.gradle**
+* Included some new shortcuts.
+
+
+# Previous Changes
+
+##(May 31, 2016):
 
 Testing Implemented for the current code.
 
-#New Files
+### New Files
 
-## UiOperationsTest.java 
+**UiOperationsTest.java **
 * Ensures that the new game button properly clears the board of all placed pieces
 * Ensures that the players alternate turns
 * Ensures that, on a new game, the person who ought to go next should.
 * Ensure that at the start of a new game, the winner’s view is invisible
 * Ensure that players cannot overwrite where their opponent has gotten with their own piece.
 
-## WinConditionsTest.java
-
+**WinConditionsTest.java**
 * Ensures that a tie will be handled properly as a tie.
 * Ensures that empty tiles cannot be added after the game has ended
 * Ensures that each of the 16 possible win scenarios actually end the game.
 
-
-# Previous Changes (May 26, 2016):
+## (May 26, 2016):
 
 Refactored code to set up an instruction/message system, and implemented precautions to work around the Life Cycle. Ultimately, the base level code and what is getting done is not all that different, but there are a few notable additions. 
 
-##Changed Files: 
+### Changed Files:
 
 **MainActivity.java**
 * Additional Life Cycle precautions have been added to ***onCreate*** and ***onSaveInstanceState***
--- app/src/main/java/com/example/bryan/tick_tack_toe/MainActivity.java
+++ app/src/main/java/com/example/bryan/tick_tack_toe/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.bryan.tick_tack_toe;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * A simple Tic-Tac-Toe game. The Main Activity creates the board, and handles all button presses.
@@ -17,80 +14,62 @@ import java.util.Scanner;
  */
 public class MainActivity extends AppCompatActivity {
 
-    // Keeps track of the turn user
-    private boolean turn;
-    private final String KEY_TURN = "turn";
-    // Keeps track of the turn number
-    private int turnCount;
-    // Keeps track if the game has ended
-    private boolean isNotFinished;
-
-    // Player Piece Strings
-    private String xValue;
-    private String oValue;
-
-    // The board state
-    private View board;
-    private int[][] boardValues;
-
     // The Match History
-    private ArrayList<String> instructions;
-    private final String KEY_INSTRUCTIONS = "match_history";
+    private ArrayList<String> mInstructions;
+    private static final String mKEY_INSTRUCTIONS = "match_history";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    // Keeps track of the mTurn user. True = Player 1, False = Player 2.
+    private boolean mTurn;
+    private static final String mKEY_TURN = "current_turn";
+
+    // Player Turn Strings
+    private String mPLAYER1;
+    private String mPLAYER2;
+
+    // Individual Game Fragments
+    private TTTFragment ticTacToe;
+
+    @Override public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        xValue = getString(R.string.xValue);
-        oValue = getString(R.string.oValue);
+        // Initialize the member variables.
+        mPLAYER1 = getString(R.string.player_1);
+        mPLAYER2 = getString(R.string.player_2);
+        mInstructions = new ArrayList<>();
+        mTurn = true;
 
-        // Initialize the match history, turn, and board variables.
-        instructions = new ArrayList<>();
-        turn = true;
-        turnCount = 9;
-        board = findViewById(R.id.board);
-        boardValues = new int[3][3];
-        isNotFinished = true;
+        // Setup the Tic-Tac-Toe fragment and inflate its layout.
+        ticTacToe = new TTTFragment();
+        ticTacToe.setArguments(getIntent().getExtras());
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, ticTacToe)
+                .commit();
 
-        // If we saved instructions, we need to recreate the board state.
+        // If we saved any Instructions, we need to recreate the board state.
         if(savedInstanceState != null) {
-            turn = savedInstanceState.getBoolean(KEY_TURN);
-            instructions = savedInstanceState.getStringArrayList(KEY_INSTRUCTIONS);
-            for(int i = 0; i < instructions.size(); i++) {
-                messageHandler(instructions.get(i));
+            mTurn = savedInstanceState.getBoolean(mKEY_TURN);
+            mInstructions = savedInstanceState.getStringArrayList(mKEY_INSTRUCTIONS);
+            for(int i = 0; i < mInstructions.size(); i++) {
+                sendMessage(mInstructions.get(i));
             }
-            // If there is an odd number of instructions, then the turn indicator will be set to
+            // If there is an odd number of Instructions, then the turn indicator will be set to
             // the wrong value. We can handle this here.
-            if(instructions.size() % 2 != 0) {
-                turn = !turn;
+            if(mInstructions.size() % 2 != 0) {
+                mTurn = !mTurn;
             }
-            isNotFinished = checkNotFinished();
+            ticTacToe.checkNotFinished();
         }
 
-
-
     }
 
-    protected void onSaveInstanceState(Bundle outState) {
+    @Override public void onSaveInstanceState(final Bundle outState) {
         super.onSaveInstanceState(outState);
-        // Saves the instructions for when the activity is called to save the instance state.
-        outState.putStringArrayList(KEY_INSTRUCTIONS, instructions);
-        outState.putBoolean(KEY_TURN, turn);
-    }
 
-    /**
-     * Sends a message alerting the event handling system that there was a tile clicked, and
-     * swaps the turn to the opposite player.
-     *
-     * @param view the tile clicked
-     */
-    public void tileOnClick(View view) {
-        String msg = getTurn() + "\n";
-        msg = msg + view.getTag().toString();
-
-        //TODO: replace this with an implemented event handling system.
-        sendMessage(msg);
+        // Store the Instructions and Turn for the save state.
+        outState.putStringArrayList(mKEY_INSTRUCTIONS, mInstructions);
+        outState.putBoolean(mKEY_TURN, mTurn);
     }
 
     /**
@@ -98,18 +77,20 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view the new game button.
      */
-    public void onNewGame(View view) {
+    public void onNewGame(final View view) {
         // Create the message.
         String msg = getTurn() + "\n";
         msg += view.getTag().toString();
 
         // Empty the instructions list, as a new game has begun.
-        instructions.clear();
-        instructions.add(msg);
+        mInstructions.clear();
+        mInstructions.add(msg);
 
         // Output New Game Message
-        String newTurn = getTurn() + "'s Turn";
-        Toast start = Toast.makeText(getApplicationContext(), "New Game! " + newTurn, Toast.LENGTH_SHORT);
+        String newTurn = (getTurn().equals(getString(R.string.player_1)) ?
+                "Player 1 (" + getString(R.string.xValue) + ")" :
+                "Player 2 (" + getString(R.string.oValue) + ")") + "'s Turn";
+        Snackbar start = Snackbar.make(findViewById(R.id.fragment_container), "New Game! " + newTurn, Snackbar.LENGTH_SHORT);
         start.show();
 
         //TODO: replace this with an implemented event handling system.
@@ -117,189 +98,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder method for a message handler / event coordinator to be implemented at a later time.
-     * @param msg the message to transmit to the message handler.
+     * Sends a message alerting the event handling system that there was a tile clicked, and
+     * swaps the mTurn to the opposite player.
+     *
+     * @param view the tile clicked
      */
-    private void sendMessage(String msg) {
-        // Keep track of instructions for recreating the board.
-        instructions.add(msg);
+    public void tileOnClick(final View view) {
+        String msg = getTurn() + "\n";
+        msg = msg + view.getTag().toString();
+
+        // Keep track of mInstructions for recreating the board.
+        mInstructions.add(msg);
 
         //TODO: replace this with an implemented event handling system.
-        messageHandler(msg);
+        sendMessage(msg);
+
+        mTurn = !mTurn;
     }
 
+
     /**
-     * Translates the messages sent by the event system and handles the individual clicks
-     * based on messages sent.
+     * A placeholder method for a message handler / event coordinator to be implemented at a later time.
      *
-     * @param msg the message to be handled.
+     * @param msg the message to transmit to the message handler.
      */
-    private void messageHandler(String msg) {
-        //TODO: Modify this when an implemented event handling system is implemented.
-        Scanner input = new Scanner(msg);
-        String player = input.nextLine();
-        String buttonTag = input.nextLine();
-        input.close();
-
-        // Call appropriate methods for each button.
-        if(buttonTag.equals("NewGame")) {
-            handleNewGame(player, buttonTag);
-        } else {
-            handleTileClick(player, buttonTag);
-        }
-
+    private void sendMessage(final String msg) {
+        //TODO: replace this with an implemented event handling system.
+        // This will be a switch for each of the individual game fragment handlers.
+        ticTacToe.messageHandler(msg);
     }
 
     /**
-     * Empties the instructions, board tiles, and outputs a new game message.
+     * Gets the current mTurn and returns a string reflecting the player's
+     * name who is currently playing.
      *
-     * @param player the player who initiated the click.
-     * @param buttonTag the tag of the button clicked.
-     */
-    private void handleNewGame(String player, String buttonTag) {
-        // Reset initial variables.
-        turnCount = 9;
-        isNotFinished = true;
-        TextView Winner = (TextView) findViewById(R.id.Winner);
-        Winner.setVisibility(View.INVISIBLE);
-
-        // Set values for each tile to empty.
-        ((Button) board.findViewWithTag("button00")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button01")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button02")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button10")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button11")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button12")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button20")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button21")).setText(getString(R.string.spaceValue));
-        ((Button) board.findViewWithTag("button22")).setText(getString(R.string.spaceValue));
-
-        evaluateBoard();
-    }
-
-    /**
-     * Assigns a value to a button without text, either X or O, depending on the player whose turn it is.
-     *
-     * @param player the player who initiated the click.
-     * @param buttonTag the tag of the button clicked.
-     */
-    private void handleTileClick(String player, String buttonTag) {
-        Button b = (Button) board.findViewWithTag(buttonTag);
-        isNotFinished = checkNotFinished();
-        // Only changes the value if the current value is empty and the game has not finished yet.
-        if (b.getText().toString().equals(getString(R.string.spaceValue)) && isNotFinished) {
-            b.setText(player);
-
-            turn = !turn;
-            turnCount--;
-
-            TextView turnNumber = (TextView) findViewById(R.id.turnDisplay);
-            turnNumber.setText(getTurn());
-            checkNotFinished();
-
-        }
-    }
-
-
-    /**
-     * Returns a string enumerating the player, depending on the current turn.
-     *
-     * @return R.string.xValue or R.string.oValue, depending on whose turn it is.
+     * @return player 1 or player 2, depending on the mTurn.
      */
     private String getTurn() {
-        if(turn) {
-            return xValue;
-        } else {
-            return oValue;
-        }
-    }
-
-    /**
-     * Evaluates the current state of the individual tiles of the board and stores them.
-     */
-    private void evaluateBoard() {
-        // Go through all the buttons.
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                Button currTile = (Button) board.findViewWithTag("button" + Integer.toString(i) + Integer.toString(j));
-                String tileValue = currTile.getText().toString();
-
-                // Assign each possible state for each tile as a value. The only possible values
-                // of each row (that indicate and endgame state) are 3 in a row of X or O. There
-                // is no way to get a value of 3 without getting 3 Xs, and there's no way to get a
-                // value of 6 without getting 3 Os.
-
-                // If there's an X in this button, store a 1
-                if(tileValue.equals(xValue)) {
-                    boardValues[i][j] = 1;
-                    // If there's an O in this button, store a 2
-                } else if (tileValue.equals(oValue)) {
-                    boardValues[i][j] = 2;
-                    // Otherwise, there's a space. -5 was chosen arbitrarily to keep row values unique.
-                } else {
-                    boardValues[i][j] = -5;
-                }
-            }
-        }
-    }
-
-    /**
-     * Evaluates the state of the board, determining if there are three in a row of either X or O.
-     *
-     * @return false if a player has won or if the full number of turns has occurred. true otherwise.
-     */
-    private boolean checkNotFinished() {
-        // First, we need to check on the buttons' states.
-        evaluateBoard();
-
-        // Evaluate all possible lines of 3.
-        int topRow = boardValues[0][0] + boardValues[0][1] + boardValues[0][2];
-        int midRow = boardValues[1][0] + boardValues[1][1] + boardValues[1][2];
-        int botRow = boardValues[2][0] + boardValues[2][1] + boardValues[2][2];
-
-        int startCol = boardValues[0][0] + boardValues[1][0] + boardValues[2][0];
-        int centerCol = boardValues[0][1] + boardValues[1][1] + boardValues[2][1];
-        int rightCol = boardValues[0][2] + boardValues[1][2] + boardValues[2][2];
-
-        int leftDiag = boardValues[0][0] + boardValues[1][1] + boardValues[2][2];
-        int rightDiag = boardValues[2][0] + boardValues[1][1] + boardValues [0][2];
-
-        // If any lines of 3 are equal to 3, X wins.
-        boolean xWins = (topRow == 3 || midRow == 3 || botRow == 3 || startCol == 3
-                || centerCol == 3 || rightCol == 3 || leftDiag == 3 || rightDiag == 3);
-
-        // If any lines of 3 are equal to 6, O wins.
-        boolean oWins = (topRow == 6 || midRow == 6 || botRow == 6 || startCol == 6
-                || centerCol == 6 || rightCol == 6 || leftDiag == 6 || rightDiag == 6);
-
-        // Handle the win conditions.
-        Toast winMsg;
-        TextView Winner = (TextView) findViewById(R.id.Winner);
-        Winner.setText(getText(R.string.spaceValue));
-        Winner.setVisibility(View.VISIBLE);
-        if(xWins) {
-            // Reveal Winning Messages
-            winMsg = Toast.makeText(getApplicationContext(), "Game over. " + xValue + " wins!", Toast.LENGTH_SHORT);
-            winMsg.show();
-            Winner.setText(R.string.xWins);
-            return false;
-        } else if (oWins) {
-            // Reveal Winning Messages
-            winMsg = Toast.makeText(getApplicationContext(), "Game over. " + oValue + " wins!", Toast.LENGTH_SHORT);
-            winMsg.show();
-            Winner.setText(R.string.oWins);
-            return false;
-        // If no one has one, and the turn timer has run out, end the game.
-        } else if(turnCount == 0) {
-            // Reveal Tie Messages
-            Winner.setText(R.string.tie);
-            Toast noWin = Toast.makeText(getApplicationContext(), "Tie!", Toast.LENGTH_SHORT);
-            noWin.show();
-            return false;
-        }
-
-        // If none of the conditions are met, the game has not yet ended, and we can continue.
-        return true;
+        return mTurn ? mPLAYER1 : mPLAYER2;
     }
 
 }
